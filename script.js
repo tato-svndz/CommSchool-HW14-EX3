ce8782769ca714b2e2fe1a6bacc28417de2462b1
Tato Svndz <tato.svndz@gmail.com>
2023-08-10 18:25:37 +0400

    სავარჯიშო 3

---
+++ script.js
@@ -0,0 +1,9 @@
+const table = document.body.firstElementChild.firstElementChild;
+const form = document.body.querySelector('form[name="search"]');
+
+console.log(document.getElementById('age-table'));
+console.log(document.querySelectorAll('label'));
+console.log(table.firstElementChild.firstElementChild.firstElementChild);
+console.log(document.body.querySelector('form[name="search"]'));
+console.log(form.querySelector('input[type="text"]:first-child'));
+console.log(form.querySelector('input[type="text"]:last-child'));
